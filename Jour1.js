ea038177a5529db15a3c8c85ea68852535e2d5bc
Rahmad3067 <aboozar919@gmail.com>
2021-06-14 13:42:33 +0200

    terminé avec les exercices

---
+++ Jour1.js
@@ -0,0 +1,29 @@
+console.log("Hello world");
+// Ex2
+var test = "My name is Rahmad";
+console.log(test);
+// Ex3
+var name = "Rahmad";
+console.log("Nice to meet you " + name);
+// Ex4
+var testLength = "I'm very long !";
+console.log(testLength.length);
+// Ex5
+var food = "croissant is meh";
+food = food.replace("meh", "so good");
+console.log(food);
+// Ex6
+var basic = "This is Cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLocaleLowerCase();
+console.log(basicUp);
+console.log(basicDown);
+// Ex7
+var word = "banana";
+var letters = word.split("");
+console.log(letters);
+
+// Ex8
+var age = 20;
+var template = (`i'm ${age} years old`);
+console.log(template);
